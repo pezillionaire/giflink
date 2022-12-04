20baa1265cdc8c06ff0ed93f48912af4f2af299e
pezillionaire <pezpengelly@gmail.com>
2022-12-04 14:16:09 -0800

    minor fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/giflink/": "/" });
-  // eleventyConfig.addPassthroughCopy({ "src/icon.gif": "icon.gif" });
   eleventyConfig.addPassthroughCopy({ "src/icon.gif": "/favicon.ico" });
-  // eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
   return {
     dir: {
       input: 'src',
